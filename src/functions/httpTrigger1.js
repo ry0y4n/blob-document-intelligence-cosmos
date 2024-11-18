c25ed02e067a5651179a83ed93ab82fb6fd00049
ry0y4n <rusk.ryoy4n@gmail.com>
2024-11-18 16:55:32 +0900

    impl: add di and read blob data through url, using managed id

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -1,57 +1,55 @@
 const { app } = require("@azure/functions");
 const { BlobServiceClient } = require("@azure/storage-blob");
+const DocumentIntelligence =
+    require("@azure-rest/ai-document-intelligence").default;
+const {
+    getLongRunningPoller,
+} = require("@azure-rest/ai-document-intelligence");
+
 const DefaultAzureCredential =
     require("@azure/identity").DefaultAzureCredential;
 
 const defaultAzureCredential = new DefaultAzureCredential();
 
 require("dotenv").config(); // ローカル実行時の .env ファイル読み込み用
-const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME;
+const blobStorageAccountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const blobContainerName = process.env.AZURE_STORAGE_CONTAINER_NAME;
 
 const blobServiceClient = new BlobServiceClient(
-    `https://${accountName}.blob.core.windows.net`,
+    `https://${blobStorageAccountName}.blob.core.windows.net`,
     defaultAzureCredential
 );
 
-const containerClient = blobServiceClient.getContainerClient(containerName);
+const containerClient = blobServiceClient.getContainerClient(blobContainerName);
+
+const documnetIntelligenceClient = DocumentIntelligence(
+    process.env["DOCUMENT_INTELLIGENCE_ENDPOINT"],
+    defaultAzureCredential
+);
 
 app.http("httpTrigger1", {
     methods: ["GET", "POST"],
     authLevel: "anonymous",
     handler: async (request, context) => {
         for await (const blob of containerClient.listBlobsFlat()) {
-            context.log(blob.name);
-            const blobClient = containerClient.getBlobClient(blob.name);
-            const downloadBlockBlobResponse = await blobClient.download();
-            const downloaded = (
-                await streamToBuffer(
-                    downloadBlockBlobResponse.readableStreamBody
-                )
-            ).toString();
-            context.log("Downloaded blob content:", downloaded);
-            // const localFilePath = path.join(__dirname, "download", blob.name);
-            // const writeStream = fs.createWriteStream(localFilePath);
-            // downloadBlockBlobResponse.readableStreamBody.pipe(writeStream);
-            // context.log(
-            //     `Blob ${blob.name} has been downloaded to ${localFilePath}`
-            // );
-            // context.log(`Blob name: ${blob.name}`);
+            const blobUrl = `https://${blobStorageAccountName}.blob.core.windows.net/${blobContainerName}/${blob.name}`;
+            const initialResponse = await documnetIntelligenceClient
+                .path("/documentModels/{modelId}:analyze", "prebuilt-layout")
+                .post({
+                    contentType: "application/json",
+                    body: {
+                        urlSource: blobUrl,
+                    },
+                    queryParameters: { outputContentFormat: "markdown" },
+                });
+            const poller = await getLongRunningPoller(
+                documnetIntelligenceClient,
+                initialResponse
+            );
+            const result = (await poller.pollUntilDone()).body;
+            context.log(result.analyzeResult.content);
         }
 
         return { body: "hello world" };
     },
 });
-
-async function streamToBuffer(readableStream) {
-    return new Promise((resolve, reject) => {
-        const chunks = [];
-        readableStream.on("data", (data) => {
-            chunks.push(data instanceof Buffer ? data : Buffer.from(data));
-        });
-        readableStream.on("end", () => {
-            resolve(Buffer.concat(chunks));
-        });
-        readableStream.on("error", reject);
-    });
-}
