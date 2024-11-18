c715af11c11fcda30dd24db01969264781b79b2e
ry0y4n <rusk.ryoy4n@gmail.com>
2024-11-18 17:35:11 +0900

    impl: insert cosomos db

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -5,6 +5,7 @@ const DocumentIntelligence =
 const {
     getLongRunningPoller,
 } = require("@azure-rest/ai-document-intelligence");
+const { CosmosClient } = require("@azure/cosmos");
 
 const DefaultAzureCredential =
     require("@azure/identity").DefaultAzureCredential;
@@ -14,6 +15,22 @@ const defaultAzureCredential = new DefaultAzureCredential();
 require("dotenv").config(); // ローカル実行時の .env ファイル読み込み用
 const blobStorageAccountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 const blobContainerName = process.env.AZURE_STORAGE_CONTAINER_NAME;
+const documentIntelligenceEndpoint = process.env.DOCUMENT_INTELLIGENCE_ENDPOINT;
+const cosmosDbEndpoint = process.env.COSMOS_DB_ENDPOINT;
+const cosmosDbDatabaseName = process.env.COSMOS_DATABASE_NAME;
+const cosmosDbContainerName = process.env.COSMOS_CONTAINER_NAME;
+
+// 各環境変数のバリデーション（どれか一つでも未設定の場合はエラー）
+function validateEnvironmentVariables() {
+    return (
+        blobStorageAccountName &&
+        blobContainerName &&
+        documentIntelligenceEndpoint &&
+        cosmosDbEndpoint &&
+        cosmosDbDatabaseName &&
+        cosmosDbContainerName
+    );
+}
 
 const blobServiceClient = new BlobServiceClient(
     `https://${blobStorageAccountName}.blob.core.windows.net`,
@@ -23,16 +40,34 @@ const blobServiceClient = new BlobServiceClient(
 const containerClient = blobServiceClient.getContainerClient(blobContainerName);
 
 const documnetIntelligenceClient = DocumentIntelligence(
-    process.env["DOCUMENT_INTELLIGENCE_ENDPOINT"],
+    documentIntelligenceEndpoint,
     defaultAzureCredential
 );
+
+const client = new CosmosClient({
+    endpoint: cosmosDbEndpoint,
+    aadCredentials: defaultAzureCredential,
+});
+
+const cosmosDatabase = client.database(cosmosDbDatabaseName);
+const cosmosContainer = cosmosDatabase.container(cosmosDbContainerName);
 
 app.http("httpTrigger1", {
     methods: ["GET", "POST"],
     authLevel: "anonymous",
     handler: async (request, context) => {
+        const isReady = validateEnvironmentVariables();
+        if (!isReady) {
+            return {
+                status: 500,
+                body: "Please set all environment variables",
+            };
+        }
+
+        // Blob の ファイル一覧を取得
         for await (const blob of containerClient.listBlobsFlat()) {
             const blobUrl = `https://${blobStorageAccountName}.blob.core.windows.net/${blobContainerName}/${blob.name}`;
+            // Document Intelligence で解析
             const initialResponse = await documnetIntelligenceClient
                 .path("/documentModels/{modelId}:analyze", "prebuilt-layout")
                 .post({
@@ -46,8 +81,18 @@ app.http("httpTrigger1", {
                 documnetIntelligenceClient,
                 initialResponse
             );
+
+            // 解析結果を Cosmos DB に保存
             const result = (await poller.pollUntilDone()).body;
             context.log(result.analyzeResult.content);
+
+            const item = {
+                id: blob.name,
+                content: result.analyzeResult.content,
+            };
+
+            const response = await cosmosContainer.items.upsert(item);
+            context.log(response.statusCode);
         }
 
         return { body: "hello world" };
